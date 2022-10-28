d7058cf68691cd8aab86f2bddfe4872f95816d86
니꼬 <nico@nomadcoders.co>
2022-10-28 15:56:57 +0900

    10.13 Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,35 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
