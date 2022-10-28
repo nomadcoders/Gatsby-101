5b40363f431018a50e4d971b7d68dca7a33c41ef
니꼬 <nico@nomadcoders.co>
2022-10-28 15:05:37 +0900

    10.10 Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,28 @@
+import { graphql, PageProps } from "gatsby";
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
   return (
     <Layout title="Blog">
-      <p>The most recent news from my shop.</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
